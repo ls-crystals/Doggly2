7dbf090ab553a34b1a9ba5857ed923a3d9d50c84
crystal <crystalrobertson236@gmail.com>
2019-12-19 16:47:45 -0500

    implemented error structure, (not fully functional)

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source "https://rubygems.org" do
 gem "sinatra-activerecord"
 gem "sqlite3"
 gem "rake"
+gem 'sinatra-flash', '~> 0.3.0'
 gem "sinatra"
 
 end
--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra/activerecord'
 require 'sinatra'
+require 'sinatra/flah'
 set :port, 3000
 
 set :database, {adapter: "sqlite3", database: "./doggly2.sqlite3"}
@@ -7,6 +8,22 @@ set :database, {adapter: "sqlite3", database: "./doggly2.sqlite3"}
 
 get '/' do
   erb :home
+end
+
+
+get '/login' do
+  erb :login
+end
+
+post 'login' do
+  user = User.find_by(email: params[:email])
+  given_pasword = params[:password]
+  if user.password. == given_password
+    session[:user_id]user.id
+    redirect '/profile'
+  else
+    flash[:error] = "Correct email, but wrong password. Did you mean: #{user.password}?"
+    redirect '/signup'
 end
 
 get '/signup' do
@@ -20,7 +37,14 @@ post '/signup' do
   @user.save
   redirect '/profile'
 else
-  redirect '/'
-end
+  flash[:error] = @user.errors.full_messages
+
+ end
   p params
+end
+
+
+
+get '/profile' do
+  erb :profile
 end
